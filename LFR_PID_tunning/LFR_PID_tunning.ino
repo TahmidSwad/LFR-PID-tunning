d5b28949fbb9fa69c335e07fe16f57594b51df8f
TahmidSwad <155631156+TahmidSwad@users.noreply.github.com>
2025-03-10 22:21:06 +0600

    Update LFR_PID_tunning.ino

--- LFR_PID_tunning/LFR_PID_tunning.ino
+++ LFR_PID_tunning/LFR_PID_tunning.ino
@@ -10,8 +10,8 @@ int sensorPin[7] = {A5, A8, A9, A10, A11, A12, A13};
 int s[7];
 int sum = 0, wSum = 0, error = 0, lastError = 0, leftMotorSpeed, rightMotorSpeed;
 double integral, derivative;
-double Kp = 1.2, Kd = 2.8, Ki = 0.0;
-int baseSpeed = 60;
+double Kp = 1.4, Kd = 2.2, Ki = 0.5;
+int baseSpeed = 0;
 char lastSen = 'l';
 
 void setup() {
